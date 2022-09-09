7bfd40747143bbafe3164f035356e02f91539b4e
MaK <muminkhan2000@gmail.com>
2022-09-09 21:23:10 +0530

    Implemeted api/new/regions for showing which regions are currently availiable   and api/new/region/:regionId for showing sourcesfrom a particular region

--- src/controller/api/newsAPIController.js
+++ src/controller/api/newsAPIController.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const sources = require('../../data/sources.json');
+const regions = require('../../data/regions.json');
 const sc = require('short-crypt');
 const getDataFromCheerio = require('../../utils/getDataFromCheerio');
 const getSourceImageFromCheerio = require('../../utils/getSourceImageFromCheerio');
@@ -33,6 +34,19 @@ sources.forEach(async (source) => {
     return;
   }
 });
+
+const sourceObject = async(source,baseUrl)=>{
+  const response = await axios.get(source.site);
+  const sourceImageLink =   getSourceImageFromCheerio( response.data); // image link for each source
+  const sourceUrl = baseUrl+`/${source.name.toLowerCase().replace(/ /g, '')}`; //url for each source endpoint
+  return      {SourceName: source.name,
+              SourceEndPoint:sourceUrl,
+              Image: /^((http|https):\/\/)/.test(sourceImageLink)
+              ? sourceImageLink
+              : defaultImage,
+              Region:source.region
+                }     
+}
 
 const controller = {
   getNews: (req, res) => {
@@ -84,32 +98,66 @@ const controller = {
     for (let i = 0; i < sources.length; i++) {
         let source = sources[i];
         try {
-            const response = await axios.get(source.site);
-            const sourceImageLink =   getSourceImageFromCheerio( response.data); // image link for each source
+            
             const hosturl = `${req.protocol}://${req.get('host')}`; // url of host
             const apipath = req.originalUrl.replace(req.url,''); //url of apipath
-            const sourceUrl = hosturl+apipath+`/${source.name.toLowerCase().replace(/ /g, '')}`; //url for each source endpoint
+            let sourceObj = await sourceObject(source,hosturl+apipath)
             
             sourcesWithEndPoint[
               new sc(source.site)  // use short-crypt to "hash" the url which works as unique ID
               .encryptToQRCodeAlphanumeric(source.site)
               .slice(0, 10)
-              ] = {
-                SourceName: source.name,
-                SourceEndPoint:sourceUrl,
-                Image: /^((http|https):\/\/)/.test(sourceImageLink)
-                ? sourceImageLink
-                : defaultImage,
-              };
+              ] = sourceObj
             }
         catch (error) {
               console.log({error });
                 }
+      }
+    return   res.json( sourcesWithEndPoint);
+    } ,
+  
+  getRegions: async (req,res) =>{
+    const regionsWithEndPoint = {};
+    const hosturl = `${req.protocol}://${req.get('host')}`; // url of host
+    const apipath = req.originalUrl; //url of apipath
+    regions.forEach((region)=>{
+        const regionUrl = hosturl + apipath + `/${region.name.toLowerCase().replace(/ /g, '')}`;
+        regionsWithEndPoint[ // use short-crypt to "hash" the url which works as unique ID
+        new sc(regionUrl).encryptToQRCodeAlphanumeric(regionUrl)
+        .slice(0, 10)
+        ] = {name:region.name,
+           url: hosturl + apipath + `/${region.name.toLowerCase().replace(/ /g, '')}`
+            } ;
+          })
+    return res.json(regionsWithEndPoint);    
+  } ,
+  
+  getSourcesByRegion : async (req, res) => {
+    const regionId = req.params.regionId;
+    const sourcesByRegion = {};
+    const sourcesRegion = sources.filter(
+      (source) => source.region.toLowerCase().replace(/ /g, '') == regionId
+    )
+    for (let i = 0; i < sourcesRegion.length; i++) {
+      let source = sourcesRegion[i];
+      try {
+        
+          const hosturl = `${req.protocol}://${req.get('host')}`; // url of host
+          const apipath = req.originalUrl.replace(req.url,''); //url of apipath
+          let sourceObj = await sourceObject(source,hosturl+apipath)
+          
+          sourcesByRegion[
+            new sc(source.site)  // use short-crypt to "hash" the url which works as unique ID
+            .encryptToQRCodeAlphanumeric(source.site)
+            .slice(0, 10)
+            ] = sourceObj;
+          }
+      catch (error) {
+            console.log({error });
+              }
     }
-return   res.json( sourcesWithEndPoint);
-}
-    
-   
+    return res.json(sourcesByRegion);      
+  }
 };
 
 module.exports = controller;
