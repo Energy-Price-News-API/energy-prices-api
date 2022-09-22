39b90e6be081edc4dd2f28ff71ec17705dafb4c9
Stefan <satalbot@protonmail.com>
2022-09-22 22:51:02 +0000

    Changed replaceAll() to replace()

--- src/utils/filterKeyword.js
+++ src/utils/filterKeyword.js
@@ -4,5 +4,5 @@
  * @returns string where space is replaced by %20
  */
 module.exports = function filterKeyword(keyword) {
-  return keyword.replaceAll(/\s/g, '%20');
+  return keyword.replace(/\s/g, '%20');
 };
