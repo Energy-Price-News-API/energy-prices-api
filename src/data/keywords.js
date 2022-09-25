8cc56b95cb6e798c567bf8231accc3af1c553b70
Stefan <satalbot@protonmail.com>
2022-09-25 16:37:04 +0000

    Added capitalization functionality to keywords

--- src/data/keywords.js
+++ src/data/keywords.js
@@ -1,5 +1,6 @@
-//List of keywords
-module.exports = keywords = [
+// List of keywords.
+// Keep first letter lowercase.
+let keywords = [
   'green energy',
   'energy price',
   'energy prices',
@@ -16,3 +17,19 @@ module.exports = keywords = [
   'gas prices', //new
   'petrol prices', //new
 ];
+
+capitalizeKeywords()
+
+// Capitalizes the first letter of each keyword, and adds it to the list of keywords.
+function capitalizeKeywords() {
+  let newKeywords = []
+
+  for(key of keywords) {
+    let newKey = key[0].toUpperCase() + key.slice(1);
+    newKeywords.push(newKey);
+  }
+  
+  keywords = keywords.concat(newKeywords)
+}
+
+module.exports = keywords;
