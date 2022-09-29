a53476878b9a9311ee8bc58fd88d671356a6516c
MizouziE <s.anglin@live.co.uk>
2022-09-29 23:19:58 +0200

    add suite for news sources list endpoint

--- __tests__/integration-tests/routes/news.test.js
+++ __tests__/integration-tests/routes/news.test.js
@@ -1,6 +1,7 @@
 const mockAxios = require('axios');
 const supertest = require('supertest');
 const createServer = require('../../../src/server');
+const sources = require('../../../src/data/sources.json')
 
 const app = createServer();
 const request = supertest(app);
@@ -15,3 +16,14 @@ describe('News Api Route', () => {
     expect(response.body).toBeTruthy();
   });
 });
+
+describe('News Sources Api Route', () => {
+  beforeEach(() => mockAxios.get('_'));
+
+  it('should return a list of all available sources', async () => {
+    const response = await request.get('/api/news/sources');
+    expect(mockAxios.get).toHaveBeenCalledTimes(sources.length + 1); // call until it has no more to call
+    expect(response.status).toBe(200);
+    expect(response.body).toBeTruthy();
+  })
+})
