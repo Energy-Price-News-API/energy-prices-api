a4d9754c0fb1b38a0324ff16e72e379b3a22b936
Leon <leonlafa@outlook.com>
2022-10-05 14:20:38 +0100

    refactor: standardise tests

--- __tests__/unit-tests/existingRegionMiddleware.test.js
+++ __tests__/unit-tests/existingRegionMiddleware.test.js
@@ -1,35 +1,30 @@
 const existingRegionMiddleware = require('../../src/middlewares/existingRegionMiddleware');
+const falsyValues = require("../../__mocks__/falsyValues");
 const path = require('path');
 
 describe('existingRegionMiddleware', () => {
-    const res = {};
-    const next = jest.fn();
+    const mockRes = {};
+    const mockNext = jest.fn();
 
-    res.status = jest.fn().mockReturnValue(res);
-    res.sendFile =  jest.fn()
+    mockRes.status = jest.fn().mockReturnValue(mockRes);
+    mockRes.sendFile =  jest.fn()
 
     it.each([
-        [{params: {regionId: "asia"}}],
-        [{params: {regionId: "americas"}}],
-        [{params: {regionId: "europe"}}],
-    ])('should pass control to the next middleware when a valid region Id has been sent in the request', (req) => {
-        existingRegionMiddleware(req, res, next);
-        expect(next).toHaveBeenCalled()
+        [
+            'asia',
+            'americas',
+            'europe'
+        ]
+    ])('should pass control to the next middleware when a valid region Id has been sent in the request', (value) => {
+        const mockReq = {params: {regionId: value}}
+        existingRegionMiddleware(mockReq, mockRes, mockNext);
+        expect(mockNext).toHaveBeenCalled()
     });
 
-    it.each([
-        [{params: {regionId: "africa"}}],
-        [{params: {regionId: "australia & oceania"}}],
-        [{params: {regionId: ""}}],
-        [{params: {regionId: null}}],
-        [{params: {regionId: undefined}}],
-        [{params: {regionId: false}}],
-        [{params: {regionId: true}}],
-        [{params: {regionId: 1}}],
-        [{params: {regionId: 0}}],
-        [{params: {regionId: {}}}],
-    ])('should send the "not-found.html" file to the client when an invalid region Id has been sent in the request', (req) => {
-        existingRegionMiddleware(req, res, next);
-        expect(res.sendFile).toHaveBeenCalledWith(path.resolve('./src/views/not-found.html'))
+    it.each([falsyValues])
+    ('should send the "not-found.html" file to the client when an invalid region Id has been sent in the request', (value) => {
+        const mockReq = {params: {regionId: value}}
+        existingRegionMiddleware(mockReq, mockRes, mockNext);
+        expect(mockRes.sendFile).toHaveBeenCalledWith(path.resolve('./src/views/not-found.html'))
     });
 });
