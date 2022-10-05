0a266d65f75d63371eb8c3ec310714f790b5dd75
Leon <leonlafa@outlook.com>
2022-10-05 11:56:49 +0100

    feat: add existing region middleware tests

--- src/middlewares/existingRegionMiddleware.js
+++ src/middlewares/existingRegionMiddleware.js
@@ -1,11 +1,9 @@
 const regions = require('../data/regions.json');
 const path = require('path');
 
-
 //Finds sourceId match within existingSources, if not, return 404 page not found.
 function existingRegionMiddleware(req, res, next) {
   const sourceId = req.params.regionId;
-
   const regionFound = regions.filter(
     (region) => region.name.toLowerCase().replace(/ /g, '') === sourceId
   );
