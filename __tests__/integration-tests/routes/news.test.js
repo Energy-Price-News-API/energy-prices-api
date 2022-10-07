d864843e97171a1a7c9578f702815a5b6f207e8a
Sayam Gandhak <gandhaksanyam@gmail.com>
2022-10-07 10:45:54 -0700

    Updated new test.js

--- __tests__/integration-tests/routes/news.test.js
+++ __tests__/integration-tests/routes/news.test.js
@@ -7,6 +7,17 @@ const regions = require('../../../src/data/regions.json');
 const app = createServer();
 const request = supertest(app);
 
+describe('News Api Route Sources', () => {
+  beforeEach(() => mockAxios.get('_'));
+
+  it('should return all the articles', async () => {
+    const response = await request.get('/api/news/sources');
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+    expect(response.status).toBe(200);
+    expect(response.body).toBeTruthy();
+  });
+});
+
 describe('News Api Route', () => {
   beforeEach(() => mockAxios.get('_'));
 
