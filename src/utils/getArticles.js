432337935ffad402b9e20ee1d59f46fd5427ac91
MizouziE <s.anglin@live.co.uk>
2022-10-30 23:27:15 +0100

    add method to dive into single article url
    Also tidied up sources a little

--- src/utils/getArticles.js
+++ src/utils/getArticles.js
@@ -2,10 +2,9 @@ const axios = require('axios');
 const sources = require('../data/sources.json');
 const sc = require('short-crypt');
 const getDataFromCheerio = require('./getDataFromCheerio');
+const getImage = require('./getImage');
 
 async function getArticles() {
-    const defaultImage =
-        'https://raw.githubusercontent.com/MizouziE/energy-prices-api/master/public/img/energy-prices-api-socials.png';
     let articles = {};
 
     sources.forEach(async (source) => {
@@ -14,21 +13,24 @@ async function getArticles() {
             const returnedArticles = getDataFromCheerio(response.data);
 
             returnedArticles.forEach(async (returned) => {
+                const key = new sc(returned.url)
+                    .encryptToQRCodeAlphanumeric(returned.url)
+                    .slice(0, 10)
+        
                 // now we have an array of multiple article's data to iterate through
                 articles[
-                    new sc(returned.url)
-                        .encryptToQRCodeAlphanumeric(returned.url)
-                        .slice(0, 10)
-                ] = {
+                    key
+                    ] = {
                     // use short-crypt to "hash" the url which works as unique ID
                     title: returned.title,
                     url: source.base + returned.url,
                     source: source.name,
-                    image: /^((http|https):\/\/)/.test(returned.image)
-                        ? returned.image
-                        : defaultImage,
                     region: source.region,
                 };
+
+                const articleUrl = Object.getOwnPropertyDescriptor(articles[key], 'url')
+
+                articles[key] = { ...articles[key], image: await getImage(articleUrl.value) }
             });
         } catch (error) {
             console.log({ error });
