13107bcbd045ff45a7f745f079914c23afc36c3d
MizouziE <s.anglin@live.co.uk>
2022-11-01 10:08:30 +0100

    add meta[og:image] tag to mock axios call

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -8,6 +8,7 @@ module.exports = {
           <meta charset="UTF-8">
           <meta http-equiv="X-UA-Compatible" content="IE=edge">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta property="og:image" content="https://www.telegraph.co.uk/bills-and-utilities/renewable-energy/" />
           <title>Mock Data</title>
         </head>
         <body>
